14521ada7bf29d360d49636190a3679a0ece66ee
Supanut <Supanut_m@cmu.ac.th>
2024-05-03 23:53:39 +0700

    edit app.vue

--- Assistance_mate_frontend/src/App.vue
+++ Assistance_mate_frontend/src/App.vue
@@ -1,5 +1,25 @@
 <template>
   <div>
-    sadasdsa
+    <router-view />
   </div>
 </template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+
+}
+</style>
