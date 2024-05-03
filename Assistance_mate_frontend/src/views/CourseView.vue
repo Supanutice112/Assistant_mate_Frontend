9e0b40355a8d252d425238ec5a7ab1f978e3ac3b
Supanut <Supanut_m@cmu.ac.th>
2024-05-04 00:15:32 +0700

    make Course.vue

--- Assistance_mate_frontend/src/views/CourseView.vue
+++ Assistance_mate_frontend/src/views/CourseView.vue
@@ -1,5 +1,44 @@
 <template>
     <div>
-        <h1>Course</h1>
+      <div class="overflow-x-auto">
+        <table class="table">
+          <!-- head -->
+          <thead>
+            <tr>
+              <th></th>
+              <th>Course</th>
+              <th>Professor</th>
+              <th>Time</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <!-- row 1 -->
+            <tr v-for="index in 5" :key="index">
+              <th>{{ index }}</th>
+              <td>
+                <div class="flex items-center gap-3">
+                  <div>
+                    <div class="font-bold">PROGRAMMING LOGICAL THINKING</div>
+                    <div class="text-sm opacity-50">511021</div>
+                  </div>
+                </div>
+              </td>
+              <td>
+                Supanut Maneeyot
+                <br />
+                <span class="badge badge-ghost badge-sm">International</span>
+              </td>
+              <td>11:00 AM - 13:00</td>
+              <th>
+                <router-link to="/attendance">
+                  <button class="btn btn-ghost btn-xs">Attendance</button>
+                </router-link>
+              </th>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
-</template>
+  </template>
+  
